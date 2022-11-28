e46ddf99d484be29ca35969db8b3775b757449ba
Deiontre10 <travontazlowry@gmail.com>
2022-11-28 16:59:00 -0500

    added variables to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,13 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // your code goes here
   var letters = "abcdefghijklmonpqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+
+  var numbersConfirm = confirm("Click OK if you would like to include numbers in your password")
+  var lettersConfirm = confirm("Click OK if you would like to include letters in your password")
+  var specialConfirm = confirm("Click OK if you would like to include special characters in your password")
+
   return "";
 }
 
