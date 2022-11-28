a26e0b9c0cf8ad984f9bf443f85c734c5c865c00
Deiontre10 <travontazlowry@gmail.com>
2022-11-28 17:42:39 -0500

    added a while loop to loop through the password length

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // your code goes here
+  // Prompt to choose how many characters in their password
+  var passwordLength = prompt("Your password must be between 8 and 256 characters how many would you like?")
+
+  while(passwordLength <= 7 || passwordLength >= 257) {
+    alert("Your password must be between 8 and 256 characters please try again");
+    var passwordLength = prompt("Your password must be between 8 and 256 characters how many would you like?")
+  }
+
   var letters = "abcdefghijklmonpqrstuvwxyz";
   var numbers = "0123456789";
   var special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 
-  var numbersConfirm = confirm("Click OK if you would like to include numbers in your password")
-  var lettersConfirm = confirm("Click OK if you would like to include letters in your password")
-  var specialConfirm = confirm("Click OK if you would like to include special characters in your password")
+  var numbersConfirm = confirm("Click OK if you would like to include numbers in your password");
+  var lettersConfirm = confirm("Click OK if you would like to include lowercase letters in your password");
+  var lettersUpperConfirm = confirm("Click OK if you would like to include uppercase letters in your password");
+  var specialConfirm = confirm("Click OK if you would like to include special characters in your password");
 
   return "";
 }
