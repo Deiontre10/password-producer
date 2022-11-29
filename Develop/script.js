39637a75d6b11621be5fab07936aeeb2a7c7522b
Deiontre10 <travontazlowry@gmail.com>
2022-11-29 16:01:06 -0500

    added if statements to script.js and declared more variables

--- Develop/script.js
+++ Develop/script.js
@@ -2,33 +2,58 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // Declare password length with a value of 0
+  var passwordLength = 0;
   // Prompt to choose how many characters in their password
   var passwordLength = prompt("Your password must be between 8 and 256 characters how many would you like?")
 
   while(passwordLength <= 7 || passwordLength >= 257) {
     alert("Your password must be between 8 and 256 characters please try again");
-    var passwordLength = prompt("Your password must be between 8 and 256 characters how many would you like?")
+    passwordLength();
   }
 
-  if(passwordLength != Number) {
-    alert("You must enter a numeric value!");
-  } else {
-    alert("Your password will be ${passwordLength} characters");
-  }
-
+  // if(passwordLength ) {
+  //   alert("You must enter a numeric value!");
+  // } else {
+  //   alert("Your password will be ${passwordLength} characters");
+  // }
+  var passphrase = "";
   var letters = "abcdefghijklmonpqrstuvwxyz";
   var numbers = "0123456789";
   var special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
   var capitalLetters = letters.toUpperCase();
+  var userOption = false;
+  var randomOption = "";
+  var passwordCharacters = "";
+
 
   var numbersConfirm = confirm("Click OK if you would like to include numbers in your password");
-
+  if(numbersConfirm == true) {
+    passwordCharacters += numbers;
+  }
 
   var lettersConfirm = confirm("Click OK if you would like to include lowercase letters in your password");
-  var lettersUpperConfirm = confirm("Click OK if you would like to include uppercase letters in your password");
-  var specialConfirm = confirm("Click OK if you would like to include special characters in your password");
+  if(lettersConfirm == true) {
+    passwordCharacters += letters;
+  }
 
-  return "";
+  var lettersUpperConfirm = confirm("Click OK if you would like to include uppercase letters in your password");
+  if(lettersUpperConfirm == true) {
+    passwordCharacters += capitalLetters;
+  }
+
+  var specialConfirm = confirm("Click OK if you would like to include special characters in your password");
+  if(specialConfirm == true) {
+    passwordCharacters += special;
+  }
+
+
+  for (var p = 0; p < passwordLength; p++) {
+    randomOption = Math.floor(Math.random() * passwordCharacters.length);
+    passphrase += passwordCharacters.substring(randomOption, randomOption +1);
+  }
+
+  return passphrase;
 }
 
 // Write password to the #password input
